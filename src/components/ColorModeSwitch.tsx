9d07c580ff239fe6f7b182ebda8e05e9f539167c
donni911 <rodion2001vo@gmail.com>
2023-05-31 15:57:53 +0300

    build the color mode switch

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,37 @@
+import {
+  HStack,
+  FormLabel,
+  FormControl,
+  Switch,
+  useColorMode,
+} from "@chakra-ui/react";
+
+function ColorModeSwitch() {
+  const { toggleColorMode, colorMode } = useColorMode();
+
+  return (
+    <HStack>
+      <FormControl
+        cursor={"pointer"}
+        display="flex"
+        gap={2}
+        alignItems="center"
+      >
+        <FormLabel htmlFor="dark-mode" m="0">
+          Light
+        </FormLabel>
+        <Switch
+          id="dark-mode"
+          colorScheme="green"
+          isChecked={colorMode === "dark"}
+          onChange={toggleColorMode}
+        />
+        <FormLabel htmlFor="dark-mode" mb="0">
+          Dark
+        </FormLabel>
+      </FormControl>
+    </HStack>
+  );
+}
+
+export default ColorModeSwitch;
--- src/theme.ts
+++ src/theme.ts
@@ -2,10 +2,12 @@ import { extendTheme, ThemeConfig } from "@chakra-ui/react";
 
 const config: ThemeConfig = {
     initialColorMode: "dark",
+   
 }
 
 const theme = extendTheme({
     config,
+    
 })
 
 export default theme
