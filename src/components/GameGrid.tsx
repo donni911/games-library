63eec410e8e5b7b9a57e1651ac84193209b60223
donni911 <rodion2001vo@gmail.com>
2023-05-31 17:04:22 +0300

    hook games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,30 +1,8 @@
-import { useState, useEffect } from "react";
-import apiClient from "../services/api-client";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 function GameGrid() {
-  const [games, setGame] = useState<Game[]>([]);
-  const [error, setError] = useState("");
+  const { games, error } = useGames();
 
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/gчames")
-      .then((response) => {
-        setGame(response.data.results);
-      })
-      .catch((error) => {
-        setError(error.message);
-      });
-  }, []);
   return (
     <ul>
       {error && <p>{error}</p>}
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+
+  const [games, setGame] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    apiClient
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
+      .then((response) => {
+        setGame(response.data.results);
+      })
+      .catch((error) => {
+        if (error instanceof CanceledError) {
+          return;
+        }
+        setError(error.message);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
