69de885a7e53dc5d1bb8dba4a968ab4709ca8ead
donni911 <rodion2001vo@gmail.com>
2023-06-01 10:16:17 +0300

    optimize images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,15 @@
-import { Card, HStack, CardBody, Image, Heading } from "@chakra-ui/react";
+import {
+  Card,
+  HStack,
+  CardBody,
+  Image,
+  Heading,
+  VStack,
+} from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 interface Props {
   game: Game;
 }
@@ -9,15 +17,21 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={"lg"} overflow={"hidden"}>
-      <Image src={game.background_image} alt={game.name} />
-      <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent={"space-between"}>
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          ></PlatformIconList>
-          <CriticScore score={game.metacritic} />
-        </HStack>
+      <Image src={getCroppedImageUrl(game.background_image)} alt={game.name} />
+      <CardBody display="flex">
+        <VStack w={"full"} alignItems={"flex-start"}>
+          <Heading fontSize="2xl">{game.name}</Heading>
+          <HStack
+            marginTop={"auto"}
+            w={"full"}
+            justifyContent={"space-between"}
+          >
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            ></PlatformIconList>
+            <CriticScore score={game.metacritic} />
+          </HStack>
+        </VStack>
       </CardBody>
     </Card>
   );
